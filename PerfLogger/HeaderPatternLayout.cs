a79cb6492d0f121118f50fab64ae55c605d295a8
Vladimir <vladimir@u-tx.com>
2015-06-26 13:51:12 +0300

    Writing csv header for every new rolled file

---
+++ PerfLogger/HeaderPatternLayout.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using log4net.Layout;
+
+namespace PerfLogger
+{
+    public class HeaderPatternLayout : PatternLayout
+    {
+        public override string Header
+        {
+            get
+            {
+                return LogSample.LogHeader + Environment.NewLine;
+            }
+        }
+    }
+}
